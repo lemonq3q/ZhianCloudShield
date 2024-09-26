91c322598dce1982fe21c2c4f08093d4065d0e48
lemonqwq <3041811612@qq.com>
2024-09-26 11:06:36 +0800

    删除注释

--- Zhian-service-IOT/src/main/java/org/example/service/impl/OneNetImpl.java
+++ Zhian-service-IOT/src/main/java/org/example/service/impl/OneNetImpl.java
@@ -1,7 +1,6 @@
 package org.example.service.impl;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.example.config.RabbitmqConfig;
 import org.example.entity.OldOnenetMsg;
 import org.example.entity.OnenetData;
@@ -62,16 +61,6 @@ public class OneNetImpl implements OneNetService {
 
 
     private void TemperatureHandler(OldOnenetMsg oldOnenetMsg){
-//        //更新温度
-//        String workshop = deviceMapper.getWorkshopByDevice_id(Integer.toString(oldOnenetMsg.getDev_id()));
-//        if(nowTemperatureMapper.getTemperatureNum(workshop)<1){
-//            nowTemperatureMapper.insertTemperature(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
-//        }else {
-//            nowTemperatureMapper.updateTemperature(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
-//        }
-//        if(oldOnenetMsg.getAt()-temperaturesMapper.getTemperatureLastTime(workshop)>2*1000){
-//            temperaturesMapper.insertTemperature(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
-//        }
         Map<String, Object> map = new HashMap<>();
         map.put("dev_id", oldOnenetMsg.getDev_id());
         map.put("value", oldOnenetMsg.getValue());
@@ -80,16 +69,6 @@ public class OneNetImpl implements OneNetService {
     }
 
     private void HumidityHandler(OldOnenetMsg oldOnenetMsg){
-//        //更新湿度
-//        String workshop = deviceMapper.getWorkshopByDevice_id(Integer.toString(oldOnenetMsg.getDev_id()));
-//        if(nowHumidityMapper.getHumidityNum(workshop)<1){
-//            nowHumidityMapper.insertHumidity(workshop,oldOnenetMsg.getAt(),Float.parseFloat(oldOnenetMsg.getValue()));
-//        }else {
-//            nowHumidityMapper.updateHumidity(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
-//        }
-//        if(oldOnenetMsg.getAt()-humiditysMapper.getHumiLastTime(workshop)>2*1000){
-//            humiditysMapper.insertHumidity(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
-//        }
         Map<String, Object> map = new HashMap<>();
         map.put("dev_id", oldOnenetMsg.getDev_id());
         map.put("value", oldOnenetMsg.getValue());
@@ -98,11 +77,6 @@ public class OneNetImpl implements OneNetService {
     }
 
     private void AttendanceHandler(OldOnenetMsg oldOnenetMsg){
-//        if(oldOnenetMsg.getDs_id().equals(FINGER)){
-//            String cardId = employeeMapper.getCardIdById(Integer.parseInt(oldOnenetMsg.getValue()));
-//            oldOnenetMsg.setValue(cardId);
-//        }
-//        AttendanceUpdate(oldOnenetMsg);
         Map<String, Object> map = new HashMap<>();
         map.put("dev_id", oldOnenetMsg.getDev_id());
         map.put("ds_id", oldOnenetMsg.getDs_id());
