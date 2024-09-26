6bfee484b8d1055c8fce34bbabf8e4a964e8409c
GR <2842056438@qq.com>
2024-09-26 09:54:25 +0800

    第六次提交

--- Zhian-api-commons/src/main/java/org/example/Main.java
+++ Zhian-api-commons/src/main/java/org/example/Main.java
@@ -1,13 +1,6 @@
 package org.example;
+System.out.println("Hello world!");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-=======
-=======
->>>>>>> cc6716667139555734c73a3bb2d974365344dbbd
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,9 +13,6 @@ public class Main {
 //        uploadFile("E:/test/img/send.png","E:/test/documents/origin.png");
 //        downloadFile("E:/test/documents/send_get.png","send.png");
         SpringApplication.run(Main.class, args);
-<<<<<<< HEAD
->>>>>>> cc6716667139555734c73a3bb2d974365344dbbd
-=======
->>>>>>> cc6716667139555734c73a3bb2d974365344dbbd
+
     }
 }
