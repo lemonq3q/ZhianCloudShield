7cae8f15318b460b3aa5315f4c2b2caf323480e9
silverstarshining <877549369@qq.com>
2024-09-26 09:21:35 +0800

    2024/9/26 9:21

--- Zhian-api-commons/src/main/java/org/example/config/RabbitmqConfig.java
+++ Zhian-api-commons/src/main/java/org/example/config/RabbitmqConfig.java
@@ -1,9 +1,11 @@
 package org.example.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+
 public class RabbitmqConfig {
     public static final String ONENET_EXCHANGE = "oneNetExchange";
     public static final String DETECT_EXCHANGE = "detectExchange";
-
 
     public static final String TEMP_QUEUE = "temperatureQueue";
     public static final String HUM_QUEUE = "humidityQueue";
@@ -16,4 +18,64 @@ public class RabbitmqConfig {
     public static final String ATD_ROUTING_KEY = "attendance";
     public static final String ONLINE_ROUTING_KEY = "onlineStatus";
     public static final String RISK_ROUTING_KEY = "risk";
+
+    //交换机定义
+    @Bean
+    DirectExchange oneNetExchange(){ return new DirectExchange(ONENET_EXCHANGE); }
+
+    @Bean
+    DirectExchange detectExchange(){ return new DirectExchange(DETECT_EXCHANGE); }
+
+    //队列定义
+    @Bean
+    public Queue temperatureQueue(){
+        return new Queue(TEMP_QUEUE);
+    }
+
+    @Bean
+    public Queue humidityQueue(){
+        return new Queue(HUM_QUEUE);
+    }
+
+    @Bean
+    public Queue attendanceQueue(){
+        return new Queue(ATD_QUEUE);
+    }
+
+    @Bean
+    public Queue onlineStatusQueue(){
+        return new Queue(ONLINE_QUEUE);
+    }
+
+    @Bean
+    public Queue riskQueue(){
+        return new Queue(RISK_QUEUE);
+    }
+
+    //绑定定义
+    @Bean
+    public Binding bindingOneNetExchange(Queue temperatureQueue, DirectExchange oneNetExchange) {
+        return BindingBuilder.bind(temperatureQueue).to(oneNetExchange).with(TEMP_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingHumidity(Queue humidityQueue, DirectExchange oneNetExchange) {
+        return BindingBuilder.bind(humidityQueue).to(oneNetExchange).with(HUM_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingOnlineStatus(Queue onlineStatusQueue, DirectExchange oneNetExchange) {
+        return BindingBuilder.bind(onlineStatusQueue).to(oneNetExchange).with(ONLINE_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingAttendance(Queue attendanceQueue, DirectExchange oneNetExchange) {
+        return BindingBuilder.bind(attendanceQueue).to(oneNetExchange).with(ATD_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding bindingRisk(Queue riskQueue, DirectExchange detectExchange) {
+        return BindingBuilder.bind(riskQueue).to(detectExchange).with(RISK_ROUTING_KEY);
+    }
+
 }
--- Zhian-service-IOT/src/main/java/org/example/service/impl/OneNetImpl.java
+++ Zhian-service-IOT/src/main/java/org/example/service/impl/OneNetImpl.java
@@ -1,9 +1,17 @@
 package org.example.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.example.config.RabbitmqConfig;
 import org.example.entity.OldOnenetMsg;
 import org.example.entity.OnenetData;
 import org.example.service.OneNetService;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
@@ -13,6 +21,9 @@ public class OneNetImpl implements OneNetService {
     private static final String FINGER = "fingerid";
     private static final String TEMPERATURE = "Temp";
     private static final String HUMIDITY = "Humi";
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
 
     @Override
     public String getAttendanceRecord(OnenetData data) {
@@ -121,7 +132,10 @@ public class OneNetImpl implements OneNetService {
 //        if(oldOnenetMsg.getAt()-temperaturesMapper.getTemperatureLastTime(workshop)>2*1000){
 //            temperaturesMapper.insertTemperature(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
 //        }
-
+        Map<String, Object> map = new HashMap<>();
+        map.put("dev_id", oldOnenetMsg.getDev_id());
+        map.put("value", oldOnenetMsg.getValue());
+        rabbitTemplate.convertAndSend(RabbitmqConfig.TEMP_ROUTING_KEY, JSON.toJSONString(map));
     }
 
     private void HumidityHandler(OldOnenetMsg oldOnenetMsg){
@@ -135,6 +149,10 @@ public class OneNetImpl implements OneNetService {
 //        if(oldOnenetMsg.getAt()-humiditysMapper.getHumiLastTime(workshop)>2*1000){
 //            humiditysMapper.insertHumidity(workshop,Float.parseFloat(oldOnenetMsg.getValue()),oldOnenetMsg.getAt());
 //        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("dev_id", oldOnenetMsg.getDev_id());
+        map.put("value", oldOnenetMsg.getValue());
+        rabbitTemplate.convertAndSend(RabbitmqConfig.HUM_ROUTING_KEY, JSON.toJSONString(map));
     }
 
     private void AttendanceHandler(OldOnenetMsg oldOnenetMsg){
@@ -143,9 +161,19 @@ public class OneNetImpl implements OneNetService {
 //            oldOnenetMsg.setValue(cardId);
 //        }
 //        AttendanceUpdate(oldOnenetMsg);
+        Map<String, Object> map = new HashMap<>();
+        map.put("dev_id", oldOnenetMsg.getDev_id());
+        map.put("ds_id", oldOnenetMsg.getDs_id());
+        map.put("value", oldOnenetMsg.getValue());
+        rabbitTemplate.convertAndSend(RabbitmqConfig.ATD_ROUTING_KEY, JSON.toJSONString(map));
     }
 
     private void DeviceHandler(OldOnenetMsg oldOnenetMsg){
+        if(oldOnenetMsg.getStatus()==1){
+            Map<String, Object> map = new HashMap<>();
+            map.put("dev_id", oldOnenetMsg.getDev_id());
+            rabbitTemplate.convertAndSend(RabbitmqConfig.ONLINE_ROUTING_KEY, JSON.toJSONString(map));
+        }
     }
 }
 
