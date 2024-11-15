8e2c2e7a68bc00c51738deecdd778d4fc25df622
lemonqwq <3041811612@qq.com>
2024-11-15 12:40:19 +0800

    微服务网关

--- Zhian-gateway/src/main/java/org/example/GatewayMain.java
+++ Zhian-gateway/src/main/java/org/example/GatewayMain.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(GatewayMain.class,args);
     }
 }
